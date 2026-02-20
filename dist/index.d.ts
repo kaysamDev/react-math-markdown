18666d6bbde235d27498e9d31ca8bec25560e751
kaysamDev <samuelmensqh@gmail.com>
2026-02-20 10:07:29 +0000

    update

--- dist/index.d.ts
+++
@@ -1,5 +0,0 @@
-export { MathMarkdown } from './components/MathMarkdown';
-export { markdownToDraftState } from './utils/markdownToDraft';
-export { draftStateToMarkdown } from './utils/draftToMarkdown';
-export { preprocessDelimiters } from './utils/delimiters';
-export type { MathMarkdownProps } from './types';
